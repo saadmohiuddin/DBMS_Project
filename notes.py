15fbca28f9a492be8763d1570ee470bb30ad6494
saadmohiuddin <saadmohiuddin@outlook.com>
2021-03-23 13:30:30 +0800

    included tutorials and assignments in notes and note.py

--- notes.py
+++ notes.py
@@ -15,20 +15,38 @@ def select_course(conn):
 course_code= select_course(myconn)
 
 currentpath=os.getcwd()+"\\notes"
-path = os.path.join(currentpath,course_code)
+path = {"lecture":os.path.join(currentpath,course_code,"lecture"), "tutorial":os.path.join(currentpath,course_code,"tutorial"),
+            "assignment":os.path.join(currentpath,course_code,"assignment")}
 
-Lecturelist=os.listdir(path) #list of files from the chosen folder
+Lecturelist=os.listdir(path["lecture"]) #list of files from the chosen folder
+tutoriallist=os.listdir(path["tutorial"])
+assignmentlist=os.listdir(path["assignment"])
 
-noteslayout = [
-            [sg.Listbox(
-            values=Lecturelist, enable_events=True, size=(40, 20), key="lecture_notelist"
-            )],
-            [sg.Listbox(
-            values=Lecturelist, enable_events=True, size=(40, 20), key="tutorial_notelist"
-            )],
-            [sg.Listbox(
-            values=Lecturelist, enable_events=True, size=(40, 20), key="assignments"
-            )]
+noteslayout =[
+
+        [
+            sg.Column(
+                [
+                        [sg.Text("Lecture Notes")],
+                        [sg.Listbox(values=Lecturelist, enable_events=True, size=(40, 20), key="lecture_notelist")]
+                ]
+            ),
+
+            sg.Column(
+                [
+                    [sg.Text("Tutorial Notes")],
+                    [sg.Listbox(values=tutoriallist, enable_events=True, size=(40, 20), key="tutorial_notelist")]
+                ]
+            ),
+
+            sg.Column(
+                [
+                    [sg.Text("assignments")],
+                    [sg.Listbox(values=assignmentlist, enable_events=True, size=(40, 20), key="assignments")]
+                ]
+            )
+
+        ]
 ]
 
 noteswindow = sg.Window("Notes", noteslayout) #incorporate this into overall window
@@ -37,8 +55,14 @@ while True:
     event,values = noteswindow.read()
 
     if event == "lecture_notelist":
+        os.system(os.path.join(path["lecture"],values[event][0])) #opening pdf file
 
-        os.system(os.path.join(path,values[event][0])) #opening pdf file
+    if event == "tutorial_notelist":
+        os.system(os.path.join(path["tutorial"],values[event][0]))
+
+    if event == "assignments":
+        os.system(os.path.join(path["assignment"],values[event][0]))
+
 
     if event == sg.WIN_CLOSED:
         break
