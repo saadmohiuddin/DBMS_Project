d6a812290c74738c89b2904a17ea55d8ee19b471
saadmohiuddin <saadmohiuddin@outlook.com>
2021-03-28 16:52:18 +0800

    updated notes.py to a function notes(course_code) that returns notesdict and pathdict

--- notes.py
+++ notes.py
@@ -2,25 +2,42 @@ import PySimpleGUI as sg
 import os
 import mysql.connector
 
+'''
 query = "SELECT course_id FROM courses WHERE course_id = 'COMP3278' "
 #query = "SELECT course_id FROM courses WHERE course_name ='Introduction to Database Management Systems' "
 
 myconn = mysql.connector.connect(host="localhost", user="root", passwd="Mysql7-4", database="facerecognition")
 def select_course(conn):
-    conn.database = "facerecognition"
-    mycursor = conn.cursor()
-    mycursor.execute(query)
-    return mycursor.fetchone()[0]
+conn.database = "facerecognition"
+mycursor = conn.cursor()
+mycursor.execute(query)
+return mycursor.fetchone()[0]
 
 course_code= select_course(myconn)
+'''
 
-currentpath=os.getcwd()+"\\notes"
-path = {"lecture":os.path.join(currentpath,course_code,"lecture"), "tutorial":os.path.join(currentpath,course_code,"tutorial"),
-            "assignment":os.path.join(currentpath,course_code,"assignment")}
+def notes(course_code):
 
-Lecturelist=os.listdir(path["lecture"]) #list of files from the chosen folder
-tutoriallist=os.listdir(path["tutorial"])
-assignmentlist=os.listdir(path["assignment"])
+
+    currentpath=os.getcwd()+"\\notes"
+
+    pathdict = {"lecture":os.path.join(currentpath,course_code,"lecture"), "tutorial":os.path.join(currentpath,course_code,"tutorial"),
+                "assignment":os.path.join(currentpath,course_code,"assignment")}
+
+    notesdict={"lecture":os.listdir(pathdict["lecture"]), "tutorial":os.listdir(pathdict["tutorial"]),
+                "assignment":os.listdir(pathdict["assignment"])}
+
+    return notesdict,pathdict
+    '''
+    Lecturelist=os.listdir(path["lecture"]) #list of files from the chosen folder
+    tutoriallist=os.listdir(path["tutorial"])
+    assignmentlist=os.listdir(path["assignment"])
+    '''
+
+
+#trial run of function notes(course_code)
+notesdict=notes("COMP3278")[0]
+pathdict= notes("COMP3278")[1]
 
 noteslayout =[
 
@@ -28,21 +45,21 @@ noteslayout =[
             sg.Column(
                 [
                         [sg.Text("Lecture Notes")],
-                        [sg.Listbox(values=Lecturelist, enable_events=True, size=(40, 20), key="lecture_notelist")]
+                        [sg.Listbox(values=notesdict["lecture"], enable_events=True, size=(40, 20), key="lecture_notelist")]
                 ]
             ),
 
             sg.Column(
                 [
                     [sg.Text("Tutorial Notes")],
-                    [sg.Listbox(values=tutoriallist, enable_events=True, size=(40, 20), key="tutorial_notelist")]
+                    [sg.Listbox(values=notesdict["tutorial"], enable_events=True, size=(40, 20), key="tutorial_notelist")]
                 ]
             ),
 
             sg.Column(
                 [
                     [sg.Text("assignments")],
-                    [sg.Listbox(values=assignmentlist, enable_events=True, size=(40, 20), key="assignments")]
+                    [sg.Listbox(values=notesdict["assignment"], enable_events=True, size=(40, 20), key="assignments")]
                 ]
             )
 
@@ -55,13 +72,13 @@ while True:
     event,values = noteswindow.read()
 
     if event == "lecture_notelist":
-        os.system(os.path.join(path["lecture"],values[event][0])) #opening pdf file
+        os.system(os.path.join(pathdict["lecture"],values[event][0])) #opening pdf file
 
     if event == "tutorial_notelist":
-        os.system(os.path.join(path["tutorial"],values[event][0]))
+        os.system(os.path.join(pathdict["tutorial"],values[event][0]))
 
     if event == "assignments":
-        os.system(os.path.join(path["assignment"],values[event][0]))
+        os.system(os.path.join(pathdict["assignment"],values[event][0]))
 
 
     if event == sg.WIN_CLOSED:
