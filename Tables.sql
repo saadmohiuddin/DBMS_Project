1cf173fad294ccfa486ef41611b0452d3ef58b3b
Saalim Mohamed <saalim1997@gmail.com>
2021-04-13 07:04:44 +0500

    added missing primary keys
    -added missing primary keys
    -corrected lecture times to multivariable attributes
    -added room numbers under office address for each lecturer
    -appropriate char lengths used and data updated for length constraint

--- Tables.sql
+++ Tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Student (
   ID VARCHAR(10) NOT NULL,
-  name VARCHAR(200) NOT NULL,
+  name VARCHAR(40) NOT NULL,
   email VARCHAR(50) NOT NULL,
   log_in_time TIMESTAMP,
   log_out_time TIMESTAMP,
@@ -10,50 +10,58 @@ CREATE TABLE Student (
 
 CREATE TABLE Course (
   Course_ID VARCHAR(8) NOT NULL,
-  name VARCHAR(200) NOT NULL,
-  info VARCHAR(2000) NOT NULL,
-  teachers_message VARCHAR(2000) NOT NULL,
-  path_to_materials VARCHAR(200),
+  name VARCHAR(40) NOT NULL,
+  info VARCHAR(300) NOT NULL,
+  teachers_message VARCHAR(100) NOT NULL,
+  path_to_materials VARCHAR(100),
   PRIMARY KEY (Course_ID)
 );
 
 CREATE TABLE Takes (
   ID VARCHAR(10) NOT NULL,
   Course_ID VARCHAR(8) NOT NULL,
+  PRIMARY KEY (ID, Course_ID),
   FOREIGN KEY (ID) REFERENCES Student (ID),
   FOREIGN KEY (Course_ID) REFERENCES Course (Course_ID)
 );
 
 CREATE TABLE Faculty (
   Faculty_ID VARCHAR(10) NOT NULL,
-  name VARCHAR(200) NOT NULL, 
-  lecturer BOOLEAN NOT NULL,
+  name VARCHAR(40) NOT NULL, 
   email VARCHAR(50) NOT NULL,
+  office_address VARCHAR(6) NOT NULL,
   PRIMARY KEY (Faculty_ID)
 );
 
 CREATE TABLE Teaches (
   Course_ID VARCHAR(8) NOT NULL,
   Faculty_ID VARCHAR(10) NOT NULL,
+  PRIMARY KEY (Course_ID, Faculty_ID),
   FOREIGN KEY (Faculty_ID) REFERENCES Faculty (Faculty_ID),
   FOREIGN KEY (Course_ID) REFERENCES Course (Course_ID)
 );
 
 CREATE TABLE Lecture (
   Course_ID VARCHAR(8) NOT NULL,
+  Zoom VARCHAR(128) NOT NULL,
+  PRIMARY KEY (Course_ID, Zoom),
+  FOREIGN KEY (Course_ID) REFERENCES Course (Course_ID)
+);
+
+CREATE TABLE LectTime (
+  Course_ID VARCHAR(8) NOT NULL,
   LectTime_HH INT NOT NULL,
-  LectTime_MM INT NOT NULL,
   LectTime_Duration INT NOT NULL,
   Weekday INT NOT NULL,
-  Zoom VARCHAR(128) NOT NULL,
+  PRIMARY KEY (Course_ID, Weekday),
   FOREIGN KEY (Course_ID) REFERENCES Course (Course_ID)
 );
 
 CREATE TABLE Tutorial (
   Course_ID VARCHAR(8) NOT NULL,
-  TutTime_HH INT,
-  TutTime_MM INT,
+  TutTime_HH INT NOT NULL,
   Weekday INT NOT NULL,
   Zoom VARCHAR(128),
+  PRIMARY KEY (Course_ID, Zoom),
   FOREIGN KEY (Course_ID) REFERENCES Course (Course_ID)
 );
